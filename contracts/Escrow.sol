5478ee715ce9dfcde76c1bbd1eaa6fc876f7f635
k8wi <thushar.02@gmail.com>
2024-04-17 19:25:53 +0530

    basic setup

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,13 +2,34 @@
 pragma solidity ^0.8.0;
 
 interface IERC721 {
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _id
-    ) external;
+    function transferFrom(address _from, address _to, uint256 _id) external;
 }
 
 contract Escrow {
+    address public lender;
+    address public inspector;
+    address public buyer;
+    address payable public seller;
+    address public nftAddress;
 
+    mapping(uint256 => bool) public isListed;
+
+    constructor(
+        address _nftAddress,
+        address payable _seller,
+        address _inspector,
+        address _lender
+    ) {
+        nftAddress = _nftAddress;
+        seller = _seller;
+        inspector = _inspector;
+        lender = _lender;
+    }
+
+    function list(uint256 _nftID) public {
+        // TransferNFRT from seller to this contract
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+
+        isListed[_nftID] = true;
+    }
 }
--- test/Escrow.js
+++ test/Escrow.js
@@ -7,4 +7,73 @@ const tokens = (n) => {
 
 describe('Escrow', () => {
 
+    let seller, buyer, inspector, lender
+    let realEstate 
+    let escrow
+
+    beforeEach(async()=>{
+        // Setup accounts
+        [buyer , seller , inspector , lender] = await ethers.getSigners()
+        
+        // Deploy Real Estate
+        const RealEstate=await ethers.getContractFactory('RealEstate')
+        realEstate =await RealEstate.deploy()
+
+        // Mint
+        let transaction = await realEstate.connect(seller).mint("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS")
+        await transaction.wait()
+
+        const Escrow = await ethers.getContractFactory('Escrow')
+        escrow = await Escrow.deploy(
+        realEstate.address,
+        seller.address,
+        inspector.address,
+        lender.address
+        )
+
+        // Approve property
+        transaction = await realEstate.connect(seller).approve(escrow.address, 1)
+        await transaction.wait()
+
+        // List Property
+        transaction = await escrow.connect(seller).list(1)
+        await transaction.wait()
+    })
+
+    describe('Deployment',()=>{
+    
+        it('Returns NFT Address', async()=>{
+            const result =await escrow.nftAddress()
+            expect(result).to.be.equal(realEstate.address)
+        })
+
+        it('Returns seller', async()=>{
+           const  result = await escrow.seller()
+           expect(result).to.be.equal(seller.address)
+        })
+        
+        it('Returns inspector', async()=>{
+            const  result = await escrow.inspector()
+            expect(result).to.be.equal(inspector.address)
+        })
+
+        it('Returns lender', async()=>{
+            const  result = await escrow.lender()
+           expect(result).to.be.equal(lender.address)
+        })
+    
+    })
+
+    describe('Listing',()=>{
+    
+        it('Updates Ownership', async()=>{
+            expect(await realEstate.ownerOf(1)).to.be.equal(escrow.address)
+        })
+    
+        it('Updates as listed', async()=>{
+            const result= await escrow.isListed(1)
+            expect(result).to.be.equal(true)
+        })
+    })
+
 })
